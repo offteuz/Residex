4f28ef391f8e7fa27b90dca4f0860f74eb9a6dcf
Matheus Jesus de Souza <matheusxone47@gmail.com>
2024-09-25 19:14:21 -0300

    Primeiro Commit.

---
+++ src/main/java/br/com/fiap/residex/ResidexApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.residex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/residex/ResidexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.residex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
